aeb85e6da973a5a83f5c7ca3d68c884fb8e2804e
StrigZ <mirgasan200@gmail.com>
2026-04-02 14:39:02 +0300

    Conect reset filters to be

--- src/components/ads-filter/reset-filters-button.tsx
+++ src/components/ads-filter/reset-filters-button.tsx
@@ -1,8 +1,16 @@
 import { Button } from '@/components/ui/button';
+import { useAdsQueryContext } from '@/context/ads-query-provider';
 
-export default function ResetFiltersButton({}: Props) {
+export default function ResetFiltersButton() {
+    const { updateQueryParam } = useAdsQueryContext();
+
     return (
-        <Button className="bg-card text-muted-foreground h-auto p-3 font-normal">
+        <Button
+            className="bg-card text-muted-foreground h-auto p-3 font-normal"
+            onClick={() =>
+                updateQueryParam({ categories: [], needsRevision: false })
+            }
+        >
             Сбросить фильтры
         </Button>
     );
